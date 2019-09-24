6493fc434a94f471b9d6e1271f075f10cfb1304a
lpidiot <1195188852@qq.com>
2019-09-24 19:49:56 +0800

    增加详细业务(仅效果)

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,24 @@
-
 const app = getApp()
 
 Page({
   data: {
-    currentTab: 0
+    currentTab: 0,
+    coverIsShow: false,
+    showDetail: true
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 页面初始化 options为页面跳转所带来的参数
 
   },
   //滑动切换
-  swiperTab: function (e) {
+  swiperTab: function(e) {
     var that = this;
     that.setData({
       currentTab: e.detail.current
     });
   },
   //点击切换
-  clickTab: function (e) {
+  clickTab: function(e) {
     var that = this;
     if (this.data.currentTab === e.target.dataset.current) {
       return false;
@@ -26,5 +27,15 @@ Page({
         currentTab: e.target.dataset.current
       })
     }
+  },
+
+  //打开详细
+  checkDetail: function() {
+    var that = this;
+
+    that.setData({
+      coverIsShow: !that.data.coverIsShow,
+      showDetail: !that.data.showDetail
+    })
   }
 })
