15869a18d84f28925a5a49ee3c21dec2c6c604a1
lpidiot <1195188852@qq.com>
2019-09-25 20:32:58 +0800

    视图demp

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,56 @@ Page({
   data: {
     currentTab: 0,
     coverIsShow: false,
-    showDetail: true
+    showDetail: true,
+    showView: true,
+    src: {
+      word: '../../images/fileType/word.png',
+      excel: '../../images/fileType/excel.png',
+      gif: '../../images/fileType/gif.png',
+      jpg: '../../images/fileType/jpg.png',
+      pdf: '../../images/fileType/pdf.png'
+    },
+    files: [{
+        id: 1,
+        name: 'WORD文件',
+        type: 'word',
+        time: '2019-10-25',
+        imgPath: '../../images/fileType/word.png'
+      },
+      {
+        id: 2,
+        name: 'EXCEL文件',
+        type: 'excel',
+        time: '2019-10-25',
+        imgPath: '../../images/fileType/excel.png'
+
+      },
+      {
+        id: 3,
+        name: 'gif文件',
+        type: 'gif',
+        time: '2019-10-25',
+        imgPath: '../../images/fileType/gif.png'
+      },
+      {
+        id: 4,
+        name: '狗狗.jpg',
+        type: 'jpg',
+        time: '2019-10-25',
+        imgPath: '../../images/fileType/jpg.png',
+        path: 'https://s2.ax1x.com/2019/09/25/uZrzdS.jpg'
+      },
+      {
+        id: 5,
+        name: 'pdf文件',
+        type: 'pdf',
+        time: '2019-10-25',
+        imgPath: '../../images/fileType/pdf.png'
+      },
+    ],
+
+    viewType:'list'
+
   },
   onLoad: function(options) {
     // 页面初始化 options为页面跳转所带来的参数
@@ -39,7 +88,36 @@ Page({
     })
   },
   //多选文件
-  selectFile:function(){
-    
+  selectFile: function() {
+
+  },
+  openImage: function(e) {
+
+    var path = e.currentTarget.dataset.path;
+    var paths = [];
+    paths.push(path);
+    console.log(paths);
+
+    wx.previewImage({
+      //current: paths // 当前显示图片的http链接
+      urls: paths // 需要预览的图片http链接列表
+    })
+  },
+  //视图选项
+  checkView: function() {
+    var that = this;
+    that.setData({
+      coverIsShow: !that.data.coverIsShow,
+      showView: !that.data.showView
+    })
+  },
+  //点击蒙层关闭各选项
+  closeView: function() {
+    var that = this;
+    that.setData({
+      coverIsShow: false,
+      showDetail: true,
+      showView: true
+    })
   }
 })
